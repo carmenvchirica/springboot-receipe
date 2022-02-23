f6ddba1c3c9daf8eddb50e80ca4dbdab3886f921
Carmen Chirica <carmen@globalsoftsys.ro>
2022-02-23 11:55:56 +0200

    @Embedded and @Embeddable objects.

---
+++ src/main/java/ch/springboot/receipe/models/Company.java
@@ -0,0 +1,63 @@
+package ch.springboot.receipe.models;
+
+import javax.persistence.*;
+
+@Entity
+public class Company {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    private String phone;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride( name = "phone", column = @Column(name = "contact_phone"))
+    })
+    private ContactPerson contactPerson;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public ContactPerson getContactPerson() {
+        return contactPerson;
+    }
+
+    public void setContactPerson(ContactPerson contactPerson) {
+        this.contactPerson = contactPerson;
+    }
+}
