7bb0287120391ef7f944281b8d3e372b9174752c
Carmen Chirica <carmen@globalsoftsys.ro>
2022-02-23 11:57:27 +0200

    Refactor

--- src/main/java/ch/springboot/receipe/models/UnitOfMeasure.java
+++ src/main/java/ch/springboot/receipe/models/UnitOfMeasure.java
@@ -14,15 +14,7 @@ public class UnitOfMeasure {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 }
